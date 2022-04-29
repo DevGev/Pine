4c26fcd459e4e14049b0ddd64a748ddccf1913fd
devgev <leifminfagel@gmail.com>
2022-04-29 17:53:52 +0200

    Fixed position boundary issue in url routing
    The route "/@" no longer matches with e.g. "/1/2" or "/abc/def", as it
    did previously. This issue stemmed from directly matching routes when
    the end of the route url was reached, naively disregarding the rest of
    the requested url.

--- pine/url.cpp
+++ pine/url.cpp
@@ -6,42 +6,45 @@ bool pine::match_url(url_t& url, const char* route_url, const char* req_url)
     size_t route_url_size = strlen(route_url);
     size_t req_url_size = strlen(req_url);
     size_t argument = 0;
-    size_t position = 0;
+    size_t req_url_pos = 0;
+    size_t route_url_pos = 0;
 
     for (size_t i = 0; i < 100; i++)
         memset(url.args[i], 0, 512);
     memset(url.url, 0, 100);
 
-    if (req_url_size > 100)
-        return false;
-
-    if (route_url_size > req_url_size)
+    if (req_url_size > 100 || route_url_size > req_url_size)
         return false;
 
     strncpy(url.url, req_url, req_url_size);
-    for (size_t i = 0; i < route_url_size; i++) {
-        if (position >= req_url_size)
-            break;
+    while (true) {
+        if (route_url[route_url_pos] == '@') {
+            if (req_url_pos >= req_url_size || req_url[req_url_pos] == '/')
+                return false;
 
-        if (route_url[i] == '@') {
             size_t pos = 0;
-            while (req_url[position] != '/') {
-                if (position >= req_url_size)
+            while (req_url[req_url_pos] != '/') {
+                if (req_url_pos >= req_url_size)
                     break;
                 if (pos >= 512)
                     return false;
-
-                url.args[argument][pos] = req_url[position];
-                position++;
+                url.args[argument][pos] = req_url[req_url_pos];
+                req_url_pos++;
                 pos++;
             }
             argument++;
-            continue;
+            route_url_pos++;
         }
 
-        if (route_url[i] != req_url[position])
+        if (req_url_pos < req_url_size && route_url_pos >= route_url_size)
             return false;
-        position++;
+        if (req_url_pos >= req_url_size && route_url_pos >= route_url_size)
+            return true;
+        if (route_url[route_url_pos] != req_url[req_url_pos])
+            return false;
+
+        req_url_pos++;
+        route_url_pos++;
     }
 
     return true;
