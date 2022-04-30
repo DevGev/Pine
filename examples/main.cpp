0904abc112e9dfbf92324881a54d07811901ab38
devgev <leifminfagel@gmail.com>
2022-05-01 01:23:47 +0200

    Update example to include information about SSL

--- examples/main.cpp
+++ examples/main.cpp
@@ -7,8 +7,10 @@ void index(pine::request* request, pine::response* response)
 
 void dynamic_image(pine::request* request, pine::response* response)
 {
-    response->set_header("Content-Type", "image/jpg");
+    /* content-type is automatically determined when calling set_content_file()
+     * however, it can also manually be changed using set_header() */
     response->set_content_file(request->arg(0));
+    response->set_header("Content-Type", "image/jpg");
 }
 
 void url_arguments(pine::request* request, pine::response* response)
@@ -24,10 +26,15 @@ void user_agent(pine::request* request, pine::response* response)
 int main()
 {
     pine::server server(5000);
+    /* ssl support requires certificate and key files,
+     * linkage with OpenSSL is also required */
+    /* server.ssl("cert.crt", "key.key"); */
+
     server.set_static_path("static");
     server.route({ "/image/@", dynamic_image });
     server.route({ "/args/@/@", url_arguments });
     server.route({ "/ua", user_agent });
     server.route({ "/", index });
+
     server.run();
 }
